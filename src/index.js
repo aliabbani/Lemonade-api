426b317c6f17d44ba22aa32f44bc86df00a1dcbb
elmar8287 <L-mar@inbox.ru>
2021-09-06 23:39:32 +0400

    Fix linter issues

--- src/index.js
+++ src/index.js
@@ -2,13 +2,9 @@ import _ from 'lodash';
 import './style.css';
 
 function component() {
-    const element = document.createElement('div');
-  
-    // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
-  
-    return element;
-  }
-  
-  document.body.appendChild(component());
+  const element = document.createElement('div');
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
+  return element;
+}
+document.body.appendChild(component());
