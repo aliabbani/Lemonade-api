9ac1b378554f705bbc60d6ccf8096aa95459eb9c
elmar8287 <L-mar@inbox.ru>
2021-09-08 20:55:15 +0400

    Add view of items with detail info

--- src/index.js
+++ src/index.js
@@ -2,5 +2,8 @@
 import _, { reduce } from 'lodash';
 import './style.css';
 import generateHome from './dom.js';
+import * as modal from './modal.js';
 
 generateHome();
+modal.showDetails();
+modal.showButton();
---
+++ src/modal.js
@@ -0,0 +1,28 @@
+const modalDetails = document.createElement('div');
+modalDetails.id = 'modal-details';
+modalDetails.className = 'meal-details';
+
+function showDetails(idMeal) {
+  const mealsCode = `
+  <div class="details-container">
+  <span id="closeDetails">X</span>
+  <h2 class="meal-title">${meal[idMeal].strMeal}</h2>
+  <img src="${meal[idMeal].strMealThumb}" alt="${
+  meal[idMeal].image.alt
+}" />
+  <div class="meal-info">
+  <p class="description">
+    ${meal[idMeal].strInstructions}
+  </p>
+  </div>
+  </div>`;
+  modalDetails.innerHTML += mealsCode;
+  document.body.appendChild(modalDetails);
+  document.getElementById('closeDetails').addEventListener('click', () => {
+    modalDetails.innerHTML = '';
+    document.body.removeChild(modalDetails);
+    blurred.forEach((section) => {
+      section.style.filter = 'blur(0)';
+    });
+  });
+}
