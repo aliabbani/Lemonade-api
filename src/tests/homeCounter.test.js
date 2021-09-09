9850fda521ed5e11d545e6c0ed486f3ab80c0842
ali abbani <aliox@ahlam.com>
2021-09-09 17:58:55 +0300

    Working on the test

---
+++ src/tests/homeCounter.test.js
@@ -0,0 +1,8 @@
+import homeCounter from '../homeCounter.js';
+
+
+describe('test the total number of dishes', () => {
+  test('return the correct number', () => {
+    expect(homeCounter()).toBe(6);
+  });
+});
